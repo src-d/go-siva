1b9e8dc385f823fa3953a5f68afc38f7b41bb03a
Santiago M. Mola <santi@mola.io>
2016-10-31 23:36:59 +0100

    index: add Glob(pattern) to Index. (#8)

--- reader_test.go
+++ reader_test.go
@@ -3,6 +3,7 @@ package siva
 import (
 	"io/ioutil"
 	"os"
+	"sort"
 
 	. "gopkg.in/check.v1"
 )
@@ -77,4 +78,43 @@ func (s *ReaderSuite) TestSeekAndRead(c *C) {
 
 		c.Assert(string(bytes), Equals, files[j].Body)
 	}
+}
+
+func (s *ReaderSuite) TestIndexGlob(c *C) {
+	s.testIndexGlob(c, "*/*", []string{
+		"letters/a",
+		"letters/b",
+		"letters/c",
+		"numbers/1",
+		"numbers/2",
+		"numbers/3",
+	})
+	s.testIndexGlob(c, "letters/*", []string{
+		"letters/a",
+		"letters/b",
+		"letters/c",
+	})
+	s.testIndexGlob(c, "nonexistent/*", []string{})
+}
+
+func (s *ReaderSuite) testIndexGlob(c *C, pattern string, expected []string) {
+	f, err := os.Open("fixtures/dirs.siva")
+	c.Assert(err, IsNil)
+
+	r := NewReader(f)
+	i, err := r.Index()
+	c.Assert(err, IsNil)
+	c.Assert(i, HasLen, 6)
+
+	matches, err := i.Glob(pattern)
+	c.Assert(err, IsNil)
+	matchNames := []string{}
+	for _, match := range matches {
+		matchNames = append(matchNames, match.Name)
+	}
+	sort.Sort(sort.StringSlice(matchNames))
+
+	c.Assert(matchNames, DeepEquals, expected)
+
+	c.Assert(f.Close(), IsNil)
 }
